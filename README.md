86b05412d4aff0be1998378983bfa4f631d39153
Carlos <carlosliracl@gmail.com>
2022-06-20 11:19:22 -0300

    Update version + add some info to mix.exs

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ by adding `ex_machina_mongodb` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:ex_machina_mongodb, "~> 0.1.0"}
+    {:ex_machina_mongodb, "~> 0.0.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,17 +4,20 @@ defmodule ExMachinaMongo.MixProject do
   def project do
     [
       app: :ex_machina_mongo,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
-      ]
+      ],
+      source_url: "https://github.com/carlosliracl/ex_machina_mongo"
     ]
   end
 
@@ -31,6 +34,17 @@ defmodule ExMachinaMongo.MixProject do
       {:ex_machina, "~> 2.0"},
       {:mongodb_driver, "~> 0.9.1", only: :test},
       {:excoveralls, "~> 0.10", only: :test}
+    ]
+  end
+
+  defp description() do
+    "Just some lines of code to make ex_machina work with the Repo module present in the elixir-mongodb-driver"
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/carlosliracl/ex_machina_mongo"}
     ]
   end
 end
